d80c1759b749cef36f2d9b98f6e9865ea216811c
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-08 15:29:31 +0200

    Update routes.tsx

--- src-frontend/routes.tsx
+++ src-frontend/routes.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { IndexPage } from "./pages";
+import { ProjectIndexPage } from "./pages/project";
+import { ROUTES } from "./utils/constants";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<IndexPage />} />
+      <Route path={ROUTES.project} element={<ProjectIndexPage />} />
     </Routes>
   );
 };
