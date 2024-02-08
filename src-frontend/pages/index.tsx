a38c5a84fc1b498caed7b3f602c9a6812c1d1672
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-08 15:29:25 +0200

    Update index.tsx

--- src-frontend/pages/index.tsx
+++ src-frontend/pages/index.tsx
@@ -1,7 +1,9 @@
 import { Heading } from "@chakra-ui/layout";
 import { Box, Button, Stack } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 export const IndexPage = () => {
+  const navigate = useNavigate();
   return (
     <Box maxWidth={"800px"} margin={"0 auto"}>
       <Stack
@@ -11,7 +13,7 @@ export const IndexPage = () => {
       >
         <Heading size={"md"}>Previous projects</Heading>
 
-        <Button>+ Add</Button>
+        <Button onClick={() => navigate("/project/unnamed")}>+ Add</Button>
       </Stack>
     </Box>
   );
