5c088de68eaa3ea15fa9bcc1aabe8989050efdd7
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-08 20:45:26 +0200

    Delete query_dataset.py

--- src-server/src/query_dataset.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
-from orm import Base, Session
-
-
-class Project(Base):
-    __tablename__ = "model"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=True)
