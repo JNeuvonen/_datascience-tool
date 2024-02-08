0c0402c1811182c1282acda09fa283069dcbf86a
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-08 15:29:21 +0200

    Create GenericErrorPage.tsx

---
+++ src-frontend/components/GenericErrorPage.tsx
@@ -0,0 +1,9 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+export const UnexpectedErrorPage = () => {
+  return (
+    <Box>
+      <Heading>Unexpected error happened</Heading>
+    </Box>
+  );
+};
