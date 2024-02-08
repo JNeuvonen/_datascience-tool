682b11a6f501332a3e27148d626f3de4ea4d2555
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-08 20:45:29 +0200

    Update route_project.py

--- src-server/src/route_project.py
+++ src-server/src/route_project.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 import pandas as pd
 
 from decorators import http_response
+from query_datafile import DatafileQuery
 from query_project import ProjectQuery
 from request_types import BodyCreateProject, BodyUploadDataset
+from utils import get_datafile_metadata
 
 
 router = APIRouter()
@@ -11,14 +13,22 @@ router = APIRouter()
 
 class RoutePaths:
     ROOT = "/"
-    UPLOAD_DATASET = "/{project_id}/dataset"
+    UPLOAD_DATASET = "/{project_name}/dataset"
 
 
 @router.post(RoutePaths.UPLOAD_DATASET)
 @http_response()
-def route_dataset(project_id: int, body: BodyUploadDataset):
+def route_dataset(project_name: int, body: BodyUploadDataset):
+    project = ProjectQuery.retrieve_project(project_name, "name")
+
+    if project is None:
+        raise HTTPException(status_code=400, detail="Incorrect project ID")
+
+    id = DatafileQuery.create_datafile_entry(
+        get_datafile_metadata(body.datasetPath, project.id)
+    )
     df = pd.read_csv(body.datasetPath)
-    return "Ok"
+    return {"id": id}
 
 
 @router.post(RoutePaths.ROOT)
