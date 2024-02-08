abd82a15929b519d91ff01367130d0d785e480e5
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-08 20:45:16 +0200

    Create SubmitBar.tsx

---
+++ src-frontend/components/SubmitBar.tsx
@@ -0,0 +1,58 @@
+import { Button } from "@chakra-ui/react";
+import { CSSProperties } from "react";
+import { BUTTON_VARIANTS } from "../theme";
+import { useKeyListener } from "../hooks/useKeyListener";
+
+interface Props {
+  cancelText?: string;
+  submitText?: string;
+  submitDisabled?: boolean;
+  hideCancel?: boolean;
+  cancelCallback?: () => void;
+  submitCallback?: () => void;
+  style?: CSSProperties;
+  mode?: "STICKY-BOTTOM" | "";
+}
+
+export const FormSubmitBar = ({
+  cancelText = "Cancel",
+  submitDisabled = false,
+  cancelCallback,
+  submitText = "Submit",
+  submitCallback,
+  style = {},
+  mode = "",
+}: Props) => {
+  const stickyBottomStyle: CSSProperties = {
+    position: "fixed",
+    bottom: 20,
+    width: `calc(100%px)`,
+  };
+
+  useKeyListener({
+    eventAction: (e) => {
+      if (e.key === "Enter" && !submitDisabled && submitCallback) {
+        submitCallback();
+      }
+    },
+  });
+
+  // Combine the styles based on the mode
+  const combinedStyle =
+    mode === "STICKY-BOTTOM" ? { ...style, ...stickyBottomStyle } : style;
+
+  return (
+    <div className="form-submit-bar" style={combinedStyle}>
+      <Button variant={BUTTON_VARIANTS.nofill} onClick={cancelCallback}>
+        {cancelText}
+      </Button>
+      <Button
+        type="submit"
+        onClick={submitCallback}
+        isDisabled={submitDisabled}
+      >
+        {submitText}
+      </Button>
+    </div>
+  );
+};
