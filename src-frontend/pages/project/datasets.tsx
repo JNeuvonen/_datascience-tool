fab3add6d914f97e499e541936692255666bcc7f
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-08 19:25:28 +0200

    Commit

--- src-frontend/pages/project/datasets.tsx
+++ src-frontend/pages/project/datasets.tsx
@@ -1,14 +1,24 @@
 import { Box, Button } from "@chakra-ui/react";
 import { open } from "@tauri-apps/api/dialog";
+import { ApiResponse } from "../../utils/request";
+import { uploadDataset } from "../../client/requests";
 
 export const ProjectDatasetsPage = () => {
   const handleFileSelection = async () => {
     const files = await open({ multiple: true });
+
+    if (Array.isArray(files)) {
+      const promises: Promise<ApiResponse>[] = [];
+      files.forEach((item) => {
+        promises.push(uploadDataset({ datasetPath: item }));
+      });
+    } else {
+    }
   };
 
   return (
     <Box>
-      <Button onClick={handleFileSelection}>test</Button>
+      <Button onClick={handleFileSelection}>+ Add</Button>
     </Box>
   );
 };
--- src-frontend/pages/project/index.tsx
+++ src-frontend/pages/project/index.tsx
@@ -1,38 +1,52 @@
-import { Box, Divider, Heading, Stack, Tabs } from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
-import { UnexpectedErrorPage } from "../../components/GenericErrorPage";
-import { Breadcrumbs } from "../../components/BreadCrumbs";
-import { PATHS } from "../../utils/path";
+import { Box, useToast } from "@chakra-ui/react";
 import { ProjectInfoPage } from "./info";
 import { ProjectDatasetsPage } from "./datasets";
 import { ChakraTabs } from "../../components/Tabs";
+import { EditableHeader } from "../../components/EditableHeader";
+import { useState } from "react";
+import { usePathParams } from "../../hooks/usePathParams";
+import { createProject } from "../../client/requests";
+import { useNavigate } from "react-router-dom";
+import { ROUTES, ROUTE_KEYS } from "../../utils/constants";
 
 const TAB_LABELS = ["Info", "Datasets"];
 const TABS = [<ProjectInfoPage key={0} />, <ProjectDatasetsPage key={1} />];
 
 export const ProjectIndexPage = () => {
-  const { project } = useParams();
+  //UTIL
+  const navigate = useNavigate();
+  const { project } = usePathParams<{ project: string }>();
+  const toast = useToast();
 
-  if (!project) {
-    return <UnexpectedErrorPage />;
-  }
+  //STATE
+  const [isUnnamed, setIsUnnamed] = useState(project === "unnamed");
+  const [projectNameInput, setProjectNameInput] = useState(project);
+
+  const updateProjectName = async () => {
+    if (isUnnamed) {
+      const res = await createProject({ name: projectNameInput });
+      if (res.status === 200) {
+        toast({
+          title: "Created project",
+          position: "top-right",
+          status: "info",
+          duration: 5000,
+          isClosable: true,
+        });
+        navigate(ROUTES.project.replace(ROUTE_KEYS.project, projectNameInput));
+      }
+    } else {
+    }
+  };
 
   return (
     <Box width={"85%"} margin={"0 auto"}>
-      <Stack
-        direction={"row"}
-        alignItems={"center"}
-        justifyContent={"space-between"}
-      >
-        <Breadcrumbs
-          items={[
-            { label: "Project", href: "/" },
-            { label: project, href: PATHS.project(project) },
-          ]}
-        />
-      </Stack>
-      <Divider style={{ marginTop: "8px" }} />
-
+      <EditableHeader
+        defaultValue={project}
+        setValue={setProjectNameInput}
+        value={projectNameInput}
+        onInputBlur={updateProjectName}
+      />
       <ChakraTabs
         labels={TAB_LABELS}
         tabs={TABS}
