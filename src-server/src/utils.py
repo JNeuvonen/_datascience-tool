8239451265bef54f3d3dfec57b7b93fc55289232
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-09 13:42:22 +0200

    Update utils.py

--- src-server/src/utils.py
+++ src-server/src/utils.py
@@ -1,9 +1,12 @@
+from contextlib import contextmanager
+import zipfile
 import os
+import shutil
 
 from decorators import LogException
 
 
-def get_datafile_metadata(path: str, project_id: int):
+def get_datafile_metadata(path: str, project_id):
     with LogException():
         file_size_bytes = os.path.getsize(path)
         file_name = get_path_last_item(path)
@@ -21,3 +24,11 @@ def get_path_last_item(path: str):
     parts = path.split("/")
 
     return parts[len(parts) - 1]
+
+
+@contextmanager
+def unzip_file(zip_path, extract_to):
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extract_to)
+    yield extract_to
+    shutil.rmtree(extract_to)
