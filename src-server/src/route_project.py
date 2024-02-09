1403ff8af21638efa678fccb3433a6595d72c803
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-09 13:42:19 +0200

    Update route_project.py

--- src-server/src/route_project.py
+++ src-server/src/route_project.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter, HTTPException
+import logging
+from fastapi import APIRouter, HTTPException, Response, status
 import pandas as pd
+from constants import DomEventChannels, Messages
 
 from decorators import http_response
-from query_datafile import DatafileQuery
+from log import get_logger
+from query_datafile import DatafileQuery, datafile_to_sql
 from query_project import ProjectQuery
-from request_types import BodyCreateProject, BodyUploadDataset
+from request_types import BodyCreateProject, BodyUploadDataset, BodyUploadDatasets
 from utils import get_datafile_metadata
 
 
@@ -14,13 +17,13 @@ router = APIRouter()
 class RoutePaths:
     ROOT = "/"
     UPLOAD_DATASET = "/{project_name}/dataset"
+    UPLOAD_DATASETS = "/{project_name}/datasets"
 
 
 @router.post(RoutePaths.UPLOAD_DATASET)
 @http_response()
-def route_dataset(project_name: int, body: BodyUploadDataset):
+def route_upload_dataset(project_name: str, body: BodyUploadDataset):
     project = ProjectQuery.retrieve_project(project_name, "name")
-
     if project is None:
         raise HTTPException(status_code=400, detail="Incorrect project ID")
 
@@ -28,7 +31,36 @@ def route_dataset(project_name: int, body: BodyUploadDataset):
         get_datafile_metadata(body.datasetPath, project.id)
     )
     df = pd.read_csv(body.datasetPath)
+    print(df.tail())
     return {"id": id}
+
+
+@router.post(RoutePaths.UPLOAD_DATASETS)
+@http_response()
+def route_upload_datasets(project_name: str, body: BodyUploadDatasets):
+    project = ProjectQuery.retrieve_project(project_name, "name")
+    logger = get_logger()
+
+    if project is None:
+        raise HTTPException(status_code=400, detail="Incorrect project ID")
+
+    idx = 1
+    for item in body.dataset_paths:
+        datafile_to_sql(project.name, project.id, item)
+        DatafileQuery.create_datafile_entry(get_datafile_metadata(item, project.id))
+        logger.log(
+            Messages.UPLOAD_FILES.format(
+                FILES_DONE=str(idx), FILES_MAX=len(body.dataset_paths)
+            ),
+            logging.INFO,
+            False,
+            False,
+        )
+        idx += 1
+
+    return Response(
+        content="OK", media_type="text/plain", status_code=status.HTTP_200_OK
+    )
 
 
 @router.post(RoutePaths.ROOT)
