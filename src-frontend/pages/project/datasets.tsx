22d7e3c1d22cd125464633ae3f10382d79ab7f7e
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-09 13:42:00 +0200

    Update datasets.tsx

--- src-frontend/pages/project/datasets.tsx
+++ src-frontend/pages/project/datasets.tsx
@@ -7,15 +7,17 @@ import {
 } from "@chakra-ui/react";
 import { open } from "@tauri-apps/api/dialog";
 import { ApiResponse } from "../../utils/request";
-import { uploadDataset } from "../../client/requests";
+import { uploadDataset, uploadDatasets } from "../../client/requests";
 import { usePathParams } from "../../hooks/usePathParams";
 import { ChakraModal } from "../../components/Modal";
 import { NameProjectModal } from "../../components/NameProjectModal";
 import { useNavigate } from "react-router-dom";
-import { ROUTES, ROUTE_KEYS } from "../../utils/constants";
-import { useEffect } from "react";
+import { DOM_EVENT_CHANNELS, ROUTES, ROUTE_KEYS } from "../../utils/constants";
+import { useEffect, useRef, useState } from "react";
 import useQueryParams from "../../hooks/useQueryParams";
 import { removeQueryParam } from "../../utils/location";
+import { UploadScreen } from "../../components/UploadFilesScreen";
+import { useMessageListener } from "../../hooks/useMessageListener";
 
 interface PageQueryParams {
   openFileSelection: string | undefined;
@@ -27,12 +29,36 @@ export const ProjectDatasetsPage = () => {
   const { openFileSelection } = useQueryParams<PageQueryParams>();
   const renameProjectModal = useDisclosure();
   const toast = useToast();
+  const fileSelectorOpenedLock = useRef(false);
+  const [uploadingFiles, setUploadingFiles] = useState(false);
+  const [loadingScreenText, setLoadingScreenText] = useState(
+    `Loading... 0 out of 7 files have been uploaded.`
+  );
+  const [loadingScreenProgress, setLoadingScreenProgress] = useState(0);
+
+  useMessageListener({
+    messageName: DOM_EVENT_CHANNELS.upload_file,
+    messageCallback: (e) => {
+      const data: { filesUploaded: number; filesMax: number } = JSON.parse(
+        e.detail
+      );
+      setLoadingScreenText(
+        `Loading... ${data.filesUploaded} out of ${data.filesMax} files have been uploaded.`
+      );
+      setLoadingScreenProgress(
+        Math.round((data.filesUploaded / data.filesMax) * 100)
+      );
+    },
+  });
 
   useEffect(() => {
     if (openFileSelection && Number(openFileSelection) === 1) {
       setTimeout(() => {
-        handleFileSelection();
-        removeQueryParam("openFileSelection");
+        if (fileSelectorOpenedLock.current == false) {
+          handleFileSelection();
+          removeQueryParam("openFileSelection");
+          fileSelectorOpenedLock.current = true;
+        }
       }, 250);
     }
   }, [openFileSelection]);
@@ -46,16 +72,28 @@ export const ProjectDatasetsPage = () => {
     const files = await open({ multiple: true });
 
     if (Array.isArray(files)) {
-      const promises: Promise<ApiResponse>[] = [];
-      files.forEach((item) => {
-        promises.push(uploadDataset({ datasetPath: item }));
-      });
+      setLoadingScreenText(
+        `Loading... 0 out of ${files.length} files have been uploaded.`
+      );
+      setUploadingFiles(true);
+      const res = await uploadDatasets(project, files);
+      if (res.status === 200) {
+        setUploadingFiles(false);
+        setLoadingScreenText("");
+      } else {
+        setUploadingFiles(false);
+      }
     } else {
     }
   };
 
   return (
     <Box>
+      <UploadScreen
+        shouldBeVisible={uploadingFiles}
+        text={loadingScreenText}
+        loadingScreenProgress={loadingScreenProgress}
+      />
       <Button onClick={handleFileSelection}>+ Add</Button>
       <ChakraModal
         {...renameProjectModal}
