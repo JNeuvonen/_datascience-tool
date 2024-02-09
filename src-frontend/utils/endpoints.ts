40cefade2980dc90f6ca57252a5a055d57878d30
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-09 13:42:04 +0200

    Update endpoints.ts

--- src-frontend/utils/endpoints.ts
+++ src-frontend/utils/endpoints.ts
@@ -3,7 +3,11 @@ const DATASET = "dataset";
 
 export const BASE_URL = "http://localhost:8000";
 
-export const URLS = {
-  project_dataset: `${BASE_URL}/${PROJECT}/${DATASET}`,
+export const REST_API_URL = {
+  project_upload_dataset: (projectName: string) =>
+    `${BASE_URL}/${PROJECT}/${projectName}/${DATASET}`,
+  project_upload_datasets: (projectName: string) =>
+    `${BASE_URL}/${PROJECT}/${projectName}/datasets`,
   project: `${BASE_URL}/${PROJECT}`,
+  log_stream: "ws://localhost:8000/streams/subscribe-log",
 };
