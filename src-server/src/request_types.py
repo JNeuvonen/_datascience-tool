4e41e99a4a6e37f6575cfd1eea61d4e4614b23ee
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-09 13:42:17 +0200

    Update request_types.py

--- src-server/src/request_types.py
+++ src-server/src/request_types.py
@@ -1,8 +1,13 @@
+from typing import List
 from pydantic import BaseModel
 
 
 class BodyUploadDataset(BaseModel):
     datasetPath: str
+
+
+class BodyUploadDatasets(BaseModel):
+    dataset_paths: List[str]
 
 
 class BodyCreateProject(BaseModel):
