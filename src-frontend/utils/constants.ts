59b0c83d8f05bd3bdd71cf259814d8b0e4ff52ea
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-09 13:42:02 +0200

    Update constants.ts

--- src-frontend/utils/constants.ts
+++ src-frontend/utils/constants.ts
@@ -5,3 +5,12 @@ export const ROUTE_KEYS = {
 export const ROUTES = {
   project: `/project/${ROUTE_KEYS.project}`,
 };
+
+export const BACKEND_STREAM_MESSAGES = {
+  upload_file: "MESSAGE_UPLOAD_FILES",
+};
+
+export const DOM_EVENT_CHANNELS = {
+  refetch_component: "refetch_component",
+  upload_file: "upload_file",
+};
