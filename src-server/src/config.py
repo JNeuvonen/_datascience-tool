55ad314fe3bd089e22b83a91fc89b33eddad8dfc
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-09 13:42:05 +0200

    Update config.py

--- src-server/src/config.py
+++ src-server/src/config.py
@@ -3,6 +3,7 @@ import os
 APP_DATA_PATH = os.getenv("APP_DATA_PATH", "")
 ENV = os.getenv("ENV", "")
 IS_TESTING = os.getenv("IS_TESTING", "")
+IS_DEBUG = os.getenv("IS_DEBUG", "")
 
 
 def is_prod():
@@ -11,6 +12,10 @@ def is_prod():
 
 def is_dev():
     return ENV == "DEV"
+
+
+def is_debug():
+    return IS_DEBUG == "DEBUG"
 
 
 def is_testing():
