72da25c99fd35897b4de00ddcc76e2283b43bc7a
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-10 10:56:32 +0200

    Commit

--- src-frontend/utils/constants.ts
+++ src-frontend/utils/constants.ts
@@ -4,10 +4,12 @@ export const ROUTE_KEYS = {
 
 export const ROUTES = {
   project: `/project/${ROUTE_KEYS.project}`,
+  settings: "/settings",
 };
 
 export const BACKEND_STREAM_MESSAGES = {
   upload_file: "MESSAGE_UPLOAD_FILES",
+  file_upload_finish: "MESSAGE_FILE_UPLOAD_FINISH",
 };
 
 export const DOM_EVENT_CHANNELS = {
@@ -17,4 +19,9 @@ export const DOM_EVENT_CHANNELS = {
 
 export const QUERY_KEYS = {
   file_upload_metadata: "file-upload-metadata",
+};
+
+export const TAURI_COMMANDS = {
+  fetch_env: "fetch_env",
+  fetch_platform: "fetch_platform",
 };
