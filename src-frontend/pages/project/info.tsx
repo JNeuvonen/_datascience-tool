c80779e32732d2056059e60723fda9082784a117
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-10 14:25:47 +0200

    Commit

--- src-frontend/pages/project/info.tsx
+++ src-frontend/pages/project/info.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@chakra-ui/react";
 
 export const ProjectInfoPage = () => {
-  return <Box>Info page</Box>;
+  return <Box marginTop={"16px"}>Info page</Box>;
 };
--- src-frontend/routes.tsx
+++ src-frontend/routes.tsx
@@ -2,12 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import { IndexPage } from "./pages";
 import { ProjectIndexPage } from "./pages/project";
 import { ROUTES } from "./utils/constants";
+import { ProjectProvider } from "./context/project";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<IndexPage />} />
-      <Route path={ROUTES.project} element={<ProjectIndexPage />} />
+
+      <Route
+        path={ROUTES.project}
+        element={
+          <ProjectProvider>
+            <ProjectIndexPage />
+          </ProjectProvider>
+        }
+      />
     </Routes>
   );
 };
