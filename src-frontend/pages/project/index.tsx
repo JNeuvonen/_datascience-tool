0596342a371d840e73ba602fa8e4d5461db546b6
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-11 13:43:43 +0200

    Commit

--- src-frontend/pages/project/index.tsx
+++ src-frontend/pages/project/index.tsx
@@ -1,79 +1,19 @@
-import {
-  Box,
-  Divider,
-  Heading,
-  MenuButton,
-  MenuItem,
-  Stack,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-  useToast,
-} from "@chakra-ui/react";
-import { ProjectInfoPage } from "./info";
+import { Box, useToast } from "@chakra-ui/react";
 import { ProjectDatasetsPage } from "./datasets";
 import { EditableHeader } from "../../components/EditableHeader";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { usePathParams } from "../../hooks/usePathParams";
 import { createProject } from "../../client/requests";
 import { useNavigate } from "react-router-dom";
 import { ROUTES, ROUTE_KEYS } from "../../utils/constants";
-import { SideBarProject } from "../../components/SideBarProject";
-import { useProjectContext } from "../../context/project";
-import { useLayoutContext } from "../../context/layout";
-import { COLOR_BG_PRIMARY_SHADE_TWO } from "../../styles/colors";
-import { Breadcrumbs } from "../../components/BreadCrumbs";
-import { ChakraMenu } from "../../components/Menu";
-import {
-  FaDownload,
-  FaFileExcel,
-  FaFileImport,
-  FaFileSignature,
-} from "react-icons/fa6";
 import { UNNAMED_PROJECT_PLACEHOLDER } from "..";
+import { useBreadcrumbs } from "../../hooks/useBreadcrumbs";
+import { ProjectMenuBar } from "../../components/project/MenuBar";
 
 export interface ProjectPageQueryParams {
   defaultTab: string | undefined;
   openFileSelection: string | undefined;
 }
-
-const TABS = [<ProjectInfoPage key={0} />, <ProjectDatasetsPage key={1} />];
-const TAB_LABELS = ["Info", "Datasets"];
-const TABS_HEIGHT = 45;
-
-const PageTabs = () => {
-  const { tabIndex, setTabIndex } = useProjectContext();
-  const { setPageTabsHeight, titleBarHeight, sideBarWidth } =
-    useLayoutContext();
-
-  useEffect(() => {
-    setPageTabsHeight(TABS_HEIGHT);
-    return () => setPageTabsHeight(0);
-  }, []);
-
-  return (
-    <Box
-      position={"fixed"}
-      top={titleBarHeight}
-      left={0}
-      bg={COLOR_BG_PRIMARY_SHADE_TWO}
-      width={`calc(100% - ${sideBarWidth}px)`}
-      marginLeft={sideBarWidth}
-      padding={"8px"}
-      zIndex={5}
-    >
-      <Tabs index={tabIndex} onChange={setTabIndex} isFitted>
-        <TabList style={{ width: "max-content" }}>
-          {TAB_LABELS.map((label, index) => (
-            <Tab key={index}>{label}</Tab>
-          ))}
-        </TabList>
-      </Tabs>
-    </Box>
-  );
-};
 
 export const ProjectIndexPage = () => {
   //UTIL
@@ -84,21 +24,14 @@ export const ProjectIndexPage = () => {
   //STATE
   const [isUnnamed] = useState(project === UNNAMED_PROJECT_PLACEHOLDER);
   const [projectNameInput, setProjectNameInput] = useState(project);
-  const { updateBreadCrumbsContent } = useLayoutContext();
-  const { selectFilesDrawer } = useProjectContext();
 
-  useEffect(() => {
-    updateBreadCrumbsContent(
-      <Breadcrumbs
-        items={[
-          { label: "New project", href: "/" },
-          { label: project, href: window.location.pathname },
-        ]}
-      />
-    );
-
-    return () => updateBreadCrumbsContent(null);
-  }, [project]);
+  useBreadcrumbs(
+    [
+      { label: "Projects", href: "/" },
+      { label: project, href: window.location.pathname },
+    ],
+    [project]
+  );
 
   const updateProjectName = async () => {
     if (isUnnamed) {
@@ -119,30 +52,13 @@ export const ProjectIndexPage = () => {
 
   return (
     <Box>
-      <SideBarProject />
       <EditableHeader
         defaultValue={project}
         setValue={setProjectNameInput}
         value={projectNameInput}
         onInputBlur={updateProjectName}
       />
-
-      <Box gap={"4px"} display={"flex"} marginLeft={"-6px"} marginTop={"2px"}>
-        <ChakraMenu menuButton={<MenuButton>File</MenuButton>}>
-          <MenuItem icon={<FaFileImport />} onClick={selectFilesDrawer.onOpen}>
-            Import data
-          </MenuItem>
-          <MenuItem icon={<FaDownload />}>Export data</MenuItem>
-          <MenuItem icon={<FaFileSignature />}>Create pivot dataframe</MenuItem>
-          <MenuItem icon={<FaFileExcel />}>Save project</MenuItem>
-        </ChakraMenu>
-        <ChakraMenu menuButton={<MenuButton>Visualize</MenuButton>}>
-          <MenuItem icon={<FaFileImport />}>Import data</MenuItem>
-          <MenuItem icon={<FaDownload />}>Export data</MenuItem>
-          <MenuItem icon={<FaFileExcel />}>Save project</MenuItem>
-        </ChakraMenu>
-      </Box>
-
+      <ProjectMenuBar />
       <ProjectDatasetsPage />
     </Box>
   );
--- src-frontend/utils/number.ts
+++ src-frontend/utils/number.ts
@@ -2,6 +2,33 @@ export const formatToGigaBytes = (sizeBytes: number) => {
   return roundNumberDropRemaining(sizeBytes / 1024 / 1024 / 1024, 2);
 };
 
+export type BYTE_SIZE = "GB" | "MB" | "KB";
+
+const SIZE_KB = 1024;
+const SIZE_MB = SIZE_KB * 1024;
+const SIZE_GB = SIZE_MB * 1024;
+
+export const formatBytes = (sizeBytes: number, appendMagnitude = true) => {
+  let formattedSize: string | number;
+  let magnitude: string;
+
+  if (sizeBytes >= SIZE_GB) {
+    formattedSize = (sizeBytes / SIZE_GB).toFixed(2);
+    magnitude = "GB";
+  } else if (sizeBytes >= SIZE_MB) {
+    formattedSize = (sizeBytes / SIZE_MB).toFixed(2);
+    magnitude = "MB";
+  } else if (sizeBytes >= SIZE_KB) {
+    formattedSize = (sizeBytes / SIZE_KB).toFixed(2);
+    magnitude = "KB";
+  } else {
+    formattedSize = sizeBytes;
+    magnitude = "Bytes";
+  }
+
+  return appendMagnitude ? `${formattedSize} ${magnitude}` : formattedSize;
+};
+
 export const roundNumberDropRemaining = (
   num: number,
   decimalPlaces: number
