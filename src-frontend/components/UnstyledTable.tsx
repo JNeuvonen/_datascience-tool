a879a64e317afc904169f8ac603988d74b30c45e
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-11 13:43:31 +0200

    Commit

---
+++ src-frontend/components/UnstyledTable.tsx
@@ -0,0 +1,67 @@
+import {
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+
+import { CSSProperties } from "react";
+
+export type RowItem = (string | number | JSX.Element)[];
+
+interface Props {
+  columns: string[];
+  columnOnClickFunc?: (item: string) => void;
+  rowOnClickFunc?: (item: RowItem) => void;
+  rows: RowItem[];
+  containerStyles?: CSSProperties;
+}
+
+export const BasicTable = ({
+  columns,
+  columnOnClickFunc,
+  rowOnClickFunc,
+  rows,
+  containerStyles,
+}: Props) => {
+  return (
+    <TableContainer style={containerStyles}>
+      <Table size="sm" className="basic-table">
+        <Thead>
+          <Tr>
+            {columns.map((item) => (
+              <Th
+                key={item}
+                onClick={() => columnOnClickFunc?.(item)}
+                style={{
+                  cursor: columnOnClickFunc ? "pointer" : undefined,
+                }}
+              >
+                {item}
+              </Th>
+            ))}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {rows.map((row, i: number) => (
+            <Tr
+              key={i}
+              className="table-row-hover" // Add class for hover style
+              onClick={() => rowOnClickFunc?.(row)}
+              style={{
+                cursor: rowOnClickFunc ? "pointer" : undefined,
+              }}
+            >
+              {row.map((rowItem: string | number | JSX.Element, j: number) => (
+                <Td key={`${i}-${j}`}>{rowItem}</Td>
+              ))}
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
