3223c7ef84633a0dbb4277cdae11e694acf015fb
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-13 17:32:54 +0200

    Create SetJoinColModal.tsx

---
+++ src-frontend/components/project/SetJoinColModal.tsx
@@ -0,0 +1,54 @@
+import { Box, Text, useToast } from "@chakra-ui/react";
+import { TEXT_VARIANTS } from "../../theme";
+import { setJoinCol } from "../../client/requests";
+import { usePathParams } from "../../hooks/usePathParams";
+
+interface Props {
+  successCallback: () => void;
+  onClose: () => void;
+  commonColumns: string[];
+}
+
+export const SetJoinColModal = (props: Props) => {
+  const { successCallback, onClose, commonColumns } = props;
+  const { project } = usePathParams<{ project: string }>();
+  const toast = useToast();
+  const onSubmit = async (selectedColumn: string) => {
+    const res = await setJoinCol(project, selectedColumn);
+    if (res.status === 200 || res === "OK") {
+      toast({
+        title: "Created project",
+        position: "bottom-left",
+        status: "info",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
+  return (
+    <Box>
+      <Text size={"xs"} variant={TEXT_VARIANTS.plain}>
+        All the datasets share a common column that can be used as a join
+        column. Join columns are helpful in merging files and aggregating      
+          rows.
+      </Text>
+
+      <Text size={"xs"} variant={TEXT_VARIANTS.plain} marginTop={"8px"}>
+        The following columns can be used as a join column:
+      </Text>
+      <Box marginTop={"16px"}>
+        {commonColumns.map((item, idx) => {
+          return (
+            <Text
+              key={idx}
+              variant={TEXT_VARIANTS.clickable}
+              onClick={() => onSubmit(item)}
+            >
+              {item}
+            </Text>
+          );
+        })}
+      </Box>
+    </Box>
+  );
+};
