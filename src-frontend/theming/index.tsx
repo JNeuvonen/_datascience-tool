a9a8a40c03c70ae657b74617349cf89a70d1537e
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-14 16:12:52 +0200

    Remove redundant theme file

--- src-frontend/theming/index.tsx
+++ src-frontend/theming/index.tsx
@@ -1,7 +1,6 @@
 export { buttonTheme, BUTTON_VARIANTS } from "./button";
 export { textTheme, TEXT_VARIANTS } from "./text";
 export { headingTheme } from "./heading";
-export { boxTheme, BOX_VARIANTS } from "./box";
 export { cardTheme } from "./card";
 export { menuItemTheme } from "./menu";
 export { dividerTheme } from "./divider";
