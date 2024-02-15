3ffb7c81a04050e0b2598be9b533a1bd8ceb6411
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-15 16:53:25 +0200

    [FE]: Create index.tsx file for project

---
+++ src-frontend/components/project/index.tsx
@@ -0,0 +1,13 @@
+export { BottomMenu } from "./BottomMenu";
+export { BottomMenuFile } from "./BottomMenuFile.tsx";
+export { CategoricalFilter } from "./CategoricalFilter.tsx";
+export { DatasetDataGrid } from "./DataGrid.tsx";
+export { DataGridSkeleton } from "./DataGridSkeleton.tsx";
+export { ImportedFilesDrawer } from "./ImportedFilesDrawer.tsx";
+export { ProjectMenuBar } from "./MenuBar.tsx";
+export { NameProjectModal } from "./NameProjectModal.tsx";
+export { ProjectUXHelper } from "./ProjectUXHelper.tsx";
+export { SelectFilesDrawer } from "./SelectFilesDrawer.tsx";
+export { SetJoinColModal } from "./SetJoinColModal.tsx";
+export { useProjectState } from "./useProjectState";
+export { updateDatafile } from "./useProjectSubmits.tsx";
