0539429e66eec6360f1295e708da167549d23f2e
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-15 15:36:13 +0200

    Add GET and PUT routes for datafile

--- src-server/src/route_datafile.py
+++ src-server/src/route_datafile.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Body, Response, status
+from fastapi import APIRouter, Body, HTTPException, Response, status
 from decorators import HttpResponseContext
 
-from query_datafile import DatafileSchema
+from query_datafile import DatafileQuery, DatafileSchema
+from utils import rename_project_file
 
 
 class RoutePaths:
@@ -15,6 +16,24 @@ router = APIRouter()
 @router.put(RoutePaths.PUT)
 async def route_put_file_by_name(datafile: DatafileSchema = Body(...)):
     with HttpResponseContext():
+        datafile_from_db = DatafileQuery.retrieve(datafile.id)
+
+        if datafile_from_db is None:
+            raise HTTPException(
+                status_code=400, detail=f"Datafile was not found for id {datafile.id}"
+            )
+
+        if datafile_from_db.file_name is not datafile.file_name:
+            rename_project_file(datafile.id, datafile.file_name)
+
+        DatafileQuery.update_datafile(datafile)
         return Response(
             content="OK", media_type="text/plain", status_code=status.HTTP_200_OK
         )
+
+
+@router.get(RoutePaths.GET)
+async def route_get_file(id: int):
+    with HttpResponseContext():
+        datafile = DatafileQuery.retrieve(id)
+        return {"data": datafile}
