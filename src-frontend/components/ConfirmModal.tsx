c39c49fb8b4e50ca92be247e6dbe390c4f8d238a
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-02-16 09:29:50 +0200

    Create ConfirmModal.tsx

---
+++ src-frontend/components/ConfirmModal.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Button } from "@chakra-ui/react";
+import { useKeyListener } from "../hooks/useKeyListener";
+import { ChakraModal } from "./Modal";
+import { BUTTON_VARIANTS } from "../theming";
+
+interface ConfirmProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  title?: string;
+  message?: string | React.ReactNode;
+  confirmText?: string;
+  cancelText?: string;
+  cancelCallback?: () => void;
+}
+
+export const ConfirmModal: React.FC<ConfirmProps> = ({
+  isOpen,
+  onClose,
+  title = "Confirm",
+  message = "Are you sure you want to confirm this action?",
+  confirmText = "Confirm",
+  cancelText = "Cancel",
+  onConfirm,
+  cancelCallback,
+}) => {
+  const handleKeyPress = (event: KeyboardEvent) => {
+    if (event.key === "Enter" && isOpen) {
+      onConfirm();
+    }
+  };
+
+  useKeyListener({ eventAction: handleKeyPress, addToDom: isOpen });
+
+  return (
+    <ChakraModal
+      isOpen={isOpen}
+      onClose={onClose}
+      title={title}
+      modalContentStyle={{
+        marginTop: "25%",
+      }}
+      footerContent={
+        <>
+          <Button
+            variant={BUTTON_VARIANTS.grey2}
+            mr={3}
+            onClick={cancelCallback ? cancelCallback : onClose}
+          >
+            {cancelText}
+          </Button>
+          <Button colorScheme="blue" onClick={onConfirm}>
+            {confirmText}
+          </Button>
+        </>
+      }
+    >
+      {message}
+    </ChakraModal>
+  );
+};
