70a3d5305bb16dd81f407cdcd9f1fd670ae427ed
Jarno Neuvonen <neuvonenjarno@gmail.com>
2024-02-23 15:28:12 +0200

    [BE]: route_project cleanup

--- src-server/src/route_project.py
+++ src-server/src/route_project.py
@@ -1,31 +1,17 @@
 import asyncio
 import json
-from fastapi import APIRouter, HTTPException, Response, status
-import pandas as pd
-from decorators import HttpResponseContext
-from query_datafile import (
-    DatafileQuery,
-)
-from query_project import ProjectQuery
-from request_types import (
-    BodyCreateProject,
-    BodyGetUploadsSize,
-    BodyUploadDataset,
-    BodyUploadDatasets,
-)
-from utils import (
-    ag_grid_filters_struct_to_sql,
-    count_rows,
-    get_datafile_columns,
-    get_datafile_init_metadata,
-    get_datafile_table_name,
-    get_dataset_pagination,
-    get_join_col_actions,
-    get_sizes_of_files,
-    upload_datasets,
-)
-from config import is_testing
 
+from config import is_testing
+from decorators import HttpResponseContext
+from fastapi import APIRouter, HTTPException, Response, status
+from query_datafile import DatafileQuery
+from query_project import ProjectQuery
+from request_types import (BodyCreateProject, BodyGetUploadsSize,
+                           BodyUploadDatasets)
+from utils import (ag_grid_filters_struct_to_sql, count_rows,
+                   get_datafile_columns, get_datafile_table_name,
+                   get_dataset_pagination, get_join_col_actions,
+                   get_sizes_of_files, upload_datasets)
 
 router = APIRouter()
 
@@ -47,20 +33,6 @@ async def route_get_uploads_size(body: BodyGetUploadsSize):
     with HttpResponseContext():
         res = get_sizes_of_files(body.file_paths)
         return {"data": res}
-
-
-@router.post(RoutePaths.DATASET)
-async def route_upload_dataset(project_name: str, body: BodyUploadDataset):
-    with HttpResponseContext():
-        project = ProjectQuery.retrieve(project_name, "name")
-        if project is None:
-            raise HTTPException(status_code=400, detail="Incorrect project ID")
-
-        id = DatafileQuery.create_datafile_entry(
-            get_datafile_init_metadata(body.datasetPath, project.id)
-        )
-        df = pd.read_csv(body.datasetPath)
-        return {"id": id}
 
 
 @router.post(RoutePaths.DATASETS)
